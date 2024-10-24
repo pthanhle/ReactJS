bf0f666678484f6d0d96976c0fe99d47207a4e06
pthanhle <22110228@student.hcmute.edu.vn>
2024-10-24 15:36:33 +0700

    video83

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getDataQuiz } from "../../services/apiServices";
+import _ from "lodash";
 
 const DetailQuiz = () => {
     const params = useParams();
@@ -12,7 +13,27 @@ const DetailQuiz = () => {
 
     const fetchQuestions = async () => {
         let res = await getDataQuiz(quizId);
-        console.log("check res: ", res)
+        if (res && res.EC === 0) {
+            let raw = res.DT;
+            let data = _.chain(raw)
+                .groupBy("id")
+                .map((value, key) => {
+                    let answers = [];
+                    let questionDescription, image = null;
+                    value.forEach((item, index) => {
+                        if (index === 0) {
+                            questionDescription = item.description;
+                            image = item.image;
+                        }
+                        answers.push(item.answers);
+                    })
+                    return { questionId: key, answers, questionDescription, image }
+
+                }
+                )
+                .value();
+            console.log('data: ', data)
+        }
     }
 
     return (
