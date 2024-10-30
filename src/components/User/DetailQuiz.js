ac027d975e413e944f257cb368d1bc22d93d774b
pthanhle <22110228@student.hcmute.edu.vn>
2024-10-30 16:54:15 +0700

    video104

--- src/components/User/DetailQuiz.js
+++ src/components/User/DetailQuiz.js
@@ -155,6 +155,7 @@ const DetailQuiz = (props) => {
             <div className="right-content">
                 <RightContent
                     dataQuiz={dataQuiz}
+                    handleFinishQuiz={handleFinishQuiz}
                 />
             </div>
             <ModalResult
